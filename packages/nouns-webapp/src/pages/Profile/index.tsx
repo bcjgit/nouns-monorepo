b4384e3401930fe101570c2ef26dcdb1eab60724
Brian Johnson <bcj26@case.edu>
2021-09-30 00:26:43 -0700

    Make activity feed (#5)
    * feed works .. save for potential bug in processing vote gql query info, minor UI, and code cleanups
    
    * a few small style things
    
    * hm seems actually processing is correct

--- packages/nouns-webapp/src/pages/Profile/index.tsx
+++ packages/nouns-webapp/src/pages/Profile/index.tsx
@@ -1,13 +1,9 @@
-import { useQuery } from '@apollo/client';
-import { Row, Col , Container} from 'react-bootstrap';
-import { LoadingNoun } from '../../components/Noun';
+import { Row, Col , Container, Button} from 'react-bootstrap';
 import NounProfileCard from '../../components/NounProfileCard';
+import ProfileActivityFeed from '../../components/ProfileActivityFeed';
 import Section from '../../layout/Section';
-import { useAllProposals } from '../../wrappers/nounsDao';
-import { nounQuery } from '../../wrappers/subgraph';
 import classes from './Profile.module.css';
 
-// TODO make profile without a param redirect to directory page
 interface ProfilePageProps {
   nounId: number;
 }
@@ -15,34 +11,19 @@ interface ProfilePageProps {
 const ProfilePage: React.FC<ProfilePageProps> = props => {
   const { nounId } = props
 
-  const { data: proposals } = useAllProposals();
-  const {loading, error, data} = useQuery(nounQuery(nounId.toString()));
-
-  if (loading) {
-    return (
-      <LoadingNoun/>
-    );
-  } else if (error) {
-    return (
-      <div>
-        Error retriving noun info
-      </div>
-    );
-  } else {
-    return (
-      <Section bgColor={classes.whiteBg} fullWidth={false}>
-        <Container>
-          <Row>
-            <Col sm={4}>
-              <NounProfileCard nounId={nounId} currentCaretakerAddress={data && data.noun.owner.id}/>
-            </Col>
-            <Col sm={8}>
-              TODO noun history feed goes here
-            </Col>
-          </Row>
-        </Container>
-      </Section>
-    );
-  }
+  return (
+    <Section bgColor={classes.whiteBg} fullWidth={false}>
+      <Container>
+        <Row>
+          <Col sm={4}>
+            <NounProfileCard nounId={nounId} />
+          </Col>
+          <Col sm={8}>
+            <ProfileActivityFeed nounId={nounId}/>
+          </Col>
+        </Row>
+      </Container>
+    </Section>
+  );
 };
 export default ProfilePage;
