af3910e63c323a233a78747c59e843b34782b536
Brian <bcj26@case.edu>
2021-10-11 20:03:27 -0700

    Add noun profile pge

---
+++ packages/nouns-webapp/src/pages/Profile/index.tsx
@@ -0,0 +1,13 @@
+
+interface ProfilePageProps {
+    nounId: number;
+}
+
+const ProfilePage: React.FC<ProfilePageProps> = props => {
+
+    return (
+        <p>test</p>
+    );
+};
+
+export default ProfilePage;
